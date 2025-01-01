840cfbd66ac31b628b3cb371834f28e6e699eed6
Soufiane El Jadi <sel-jadi@e1r5p2.1337.ma>
2025-01-01 21:51:21 +0100

    finish

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,29 @@
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include  <iostream>
+#include  <cmath>
+
+class Fixed
+{
+    private:
+        int _value;
+        static const int _bits = 8;
+    public:
+        Fixed();
+        Fixed(Fixed const &_new);
+        Fixed(const int value);
+        Fixed(const float value);
+        Fixed	&operator=(Fixed const &copy);
+        int getRawBits() const;
+        void setRawBits( int const raw );
+        float toFloat( void ) const;
+        int toInt( void ) const;
+        ~Fixed();
+};
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &fixed);
+
+
+#endif
